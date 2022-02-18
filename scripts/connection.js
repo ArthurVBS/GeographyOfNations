729f73d3dad8f86b72eafd0f61545f790dd1ffab
ArthurVBS <arthurvini2703@gmail.com>
2022-02-18 14:30:42 -0300

    Inserindo ícones e estilização

--- scripts/connection.js
+++ scripts/connection.js
@@ -15,7 +15,8 @@ search.input.addEventListener('keypress', (event) => {
     }
 })
 
-function searchResults(type, name) {
+function searchResults(type, name)
+{
     fetch(`https://restcountries.com/v3.1/${type}/${name}`)
         .then(response => {
             if (!response.ok) {
@@ -35,49 +36,8 @@ function displayResults(nation)
 {
     if (search.type == 'name')
     {
-        const dataByNameSearch = {
-            div: document.querySelector('.text'),
-            flag: document.getElementById('nationFlag'),
-            name: document.getElementById('nationName'),
-            lang: document.getElementById('nationLang'),
-            region: document.getElementById('nationRegion'),
-            capital: document.createElement('h4'),
-            population: document.getElementById('nationPopulation'),
-            currenciesName: document.getElementById('nationCurrenciesName'),
-            currenciesSymbol: document.getElementById('nationCurrenciesSymbol')
-        }
-        nation = nation[0]
-
-        box = document.querySelector('.results')
-        box.setAttribute('class', 'results')
-        box.style.backgroundImage = `url(${nation.flags.png})`
-
-        dataByNameSearch.flag.innerHTML = `<img src="${nation.flags.png}" alt="Bandeira da Nação">`
-        dataByNameSearch.name.innerText = nation.translations.por.common
-
-        dataByNameSearch.capital.innerText = nation.capital
-        dataByNameSearch.capital.id = 'nationCapital'
-        dataByNameSearch.div.appendChild(dataByNameSearch.capital)
-
-        dataByNameSearch.region.innerText = nation.continents
-        dataByNameSearch.lang.innerText = Object.values(nation.languages)
-        dataByNameSearch.currenciesName.innerText = Object.values(nation.currencies)[0].name
-        dataByNameSearch.currenciesSymbol.innerText = Object.values(nation.currencies)[0].symbol
-
-        if (nation.population < 1000000)
-        {
-            dataByNameSearch.population.innerText = `${(nation.population / 1000).toFixed(1) } K`
-        }
-        else if (nation.population < 1000000000)
-        {
-            dataByNameSearch.population.innerText = `${(nation.population / 1000000).toFixed(1) } Mi`
-        }
-        else
-        {
-            dataByNameSearch.population.innerText = `${(nation.population / 1000000000).toFixed(1) } Bi`
-        }
-
-        console.log(nation)
+        resultsName(nation[0])
+        
     }
     else if (search.type == 'lang')
     {
@@ -91,4 +51,48 @@ function displayResults(nation)
     {
         console.log('Há um erro!')
     }
+}
+
+function resultsName(nation)
+{
+    const dataByNameSearch = {
+        flag: document.getElementById('nationFlag'),
+        name: document.getElementById('nationName'),
+        lang: document.getElementById('nationLang'),
+        region: document.getElementById('nationRegion'),
+        capital: document.getElementById('nationCapital'),
+        population: document.getElementById('nationPopulation'),
+        currencies: document.getElementById('nationCurrencies')
+    }
+
+    box = document.querySelector('.results')
+    box.setAttribute('class', 'results')
+    box.style.backgroundImage = `url(${nation.flags.png})`
+
+    dataByNameSearch.flag.innerHTML = `<img src="${nation.flags.png}" alt="Bandeira da Nação">`
+
+    dataByNameSearch.name.innerText = nation.translations.por.common
+
+    dataByNameSearch.capital.innerText = nation.capital.toString().replace(/,/g , ', ')
+
+    dataByNameSearch.region.innerText = nation.continents
+
+    dataByNameSearch.currencies.innerText = `${Object.values(nation.currencies)[0].name} (${Object.values(nation.currencies)[0].symbol})` 
+    
+    dataByNameSearch.lang.innerText = Object.values(nation.languages).slice(0, 3).toString().replace(/,/g , ', ')
+
+    if (nation.population < 1000000)
+    {
+        dataByNameSearch.population.innerText = `${(nation.population / 1000).toFixed(1) } K`
+    }
+    else if (nation.population < 1000000000)
+    {
+        dataByNameSearch.population.innerText = `${(nation.population / 1000000).toFixed(1) } Mi`
+    }
+    else
+    {
+        dataByNameSearch.population.innerText = `${(nation.population / 1000000000).toFixed(1) } Bi`
+    }
+
+    console.log(nation)
 }
